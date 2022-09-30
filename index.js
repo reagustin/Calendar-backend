29bc2a21bce51fe6fd752bf0c7a600ebdb1c4e02
Agustin <re_agustin@hotmail.com>
2022-09-30 20:02:55 -0300

    endpoints

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 // Directorio publico
 app.use( express.static('public') );
 
-
+// Lectura y parseo del body
+app.use( express.json());
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,15 @@
     Rutas de Usuarios / Auth
     host + /api/auth
 */
-
-
 const { Router} = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {    
-    res.json({
-        ok: true
-    })
-})
+const {crearUsuario, loginUsuario, revalidarToken} = require('../controllers/auth');
+
+router.post('/new', crearUsuario);
+
+router.post('/', loginUsuario);
+
+router.get('/renew', revalidarToken);
 
 module.exports = router;
