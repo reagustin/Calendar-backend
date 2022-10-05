957580b1597deab710f94c044423b9764f2d0d30
Agustin <re_agustin@hotmail.com>
2022-10-05 11:21:51 -0300

    crearEventosSave

--- routes/events.js
+++ routes/events.js
@@ -5,7 +5,10 @@
 
 const { Router} = require('express');
 const { validarJWT } = require('../middlewares/validar-JWT');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento} = require('../controllers/events');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -16,7 +19,12 @@ router.use(validarJWT);
 router.get('/', getEventos)
 
 // Crear evento
-router.post('/', crearEvento)
+router.post('/', [
+    check('title','El titulo es obligatorio').not().isEmpty(),
+    check('start','La fecha de inicio es obligatoria').custom(isDate),
+    check('end','La fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+] ,crearEvento)
 
 // Actualizar eventos
 router.put('/:id', actualizarEvento)
